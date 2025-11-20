f38c718b96956fca952b0001b32f7611965d6625
Cristopher <cristopherquiros2@gmail.com>
2025-11-19 19:16:49 -0600

    Rides and Bookings Migration

---
+++ database/migrations/0001_01_01_000002_create_rides_table copy.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rides', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('name');
+            $table->string('destination');
+            $table->string('arrival');
+            $table->string('date');
+            $table->string('space_cost');
+            $table->integer('space');
+
+            $table->integer('user_id');
+            $table->foreign('user_id')
+                ->references('cedula')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->integer('vehicle_id');
+
+            $table->foreign('vehicle_id')
+                ->references('id')
+                ->on('vehicles')
+                ->onDelete('cascade');
+            $table->string('status');
+
+        });
+
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->integer('id')->primary();
+
+            $table->integer('user_id');
+
+            $table->foreign('user_id')
+                ->references('cedula')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->string('status');
+
+            $table->integer('ride_id');
+
+            $table->foreign('ride_id')
+                ->references('id')
+                ->on('rides')
+                ->onDelete('cascade');
+
+            $table->string('date');
+            $table->integer('driver_id');
+
+            $table->foreign('driver_id')
+                ->references('cedula')
+                ->on('users')
+                ->onDelete('cascade');
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movements');
+        Schema::dropIfExists('bookings');
+        Schema::dropIfExists('rides');
+        Schema::dropIfExists('vehicles');
+        Schema::dropIfExists('users');
+
+    }
+};
