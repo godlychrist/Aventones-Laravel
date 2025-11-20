6e285fd273fe061e2061615f5ba6e88d5fdc3f8d
Cristopher <cristopherquiros2@gmail.com>
2025-11-19 19:16:37 -0600

    Vehicles Migration

---
+++ database/migrations/0001_01_01_000001_create_vehicles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->integer('plateNum')->unique();
+            $table->string('color');
+            $table->string('brand');
+            $table->string('model');
+            $table->date('year');
+            $table->string('image');
+            $table->integer('user_id');
+            $table->foreign('user_id')
+                ->references('cedula')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->integer('capacity');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
