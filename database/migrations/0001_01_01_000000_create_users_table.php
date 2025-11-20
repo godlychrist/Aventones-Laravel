eb0a8706f2971b344f525d83d9d93d1aca2403bb
Cristopher <cristopherquiros2@gmail.com>
2025-11-19 19:16:20 -0600

    Users Table Migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,36 +4,38 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->integer('cedula')->primary();
             $table->string('name');
+            $table->string('lastname');
+            $table->date('birthDate');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('phoneNum');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('image');
+            $table->string('state');
+            $table->string('userType');
             $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->datetime('expiration_token');
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
+        Schema::create('movements', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->integer('user_id');
+            $table->foreign('user_id')
+                  ->references('cedula')
+                  ->on('users')
+                  ->onDelete('cascade');
+            $table->date('date');
+            $table->string('leavePlace');
+            $table->string('destinationPlace');
+            $table->integer('resultsNum');
         });
     }
 
@@ -42,8 +44,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('movements');
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
     }
 };
