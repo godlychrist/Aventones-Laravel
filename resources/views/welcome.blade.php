5d31bb88b115baef64a7c7b8bbd027bec5235d09
Cristopher <cristopherquiros2@gmail.com>
2025-11-26 20:47:46 -0600

    Welcome Blade Updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,6 +77,7 @@
                 </form>
 
                 <!-- Tabla -->
+                 
                 <div class="table-responsive">
                     <table class="table table-bordered align-middle">
                         <thead class="table-light">
